3ec883b4596a15f3f1095d1e7f65e903b2b1e50b
Thimira Aloka M H <150279156+ThimiraAloka@users.noreply.github.com>
2023-12-10 21:29:58 +0530

    Customer Report Jasper Success

--- Jasper_Customer_Item_List_iCM105132/.idea/sonarlint/securityhotspotstore/index.pb
+++ Jasper_Customer_Item_List_iCM105132/.idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,7 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 j
 :src/main/java/controller/DashboardForJasperController.java,5\2\528d3c0901a4ad8cf64e3c6c1a6d8efc481c996f
+R
+"src/main/java/db/DBConnection.java,f\d\fd7336f68247f5ace91042607378ff616e4285dd
+Q
+!src/main/java/AppInitializer.java,d\8\d8724d5ff68b00b1e87def6fac487d03194f989f
--- Jasper_Customer_Item_List_iCM105132/src/main/java/controller/DashboardForJasperController.java
+++ Jasper_Customer_Item_List_iCM105132/src/main/java/controller/DashboardForJasperController.java
@@ -1,12 +1,32 @@
 package controller;
 
+import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+import java.sql.SQLException;
 
 public class DashboardForJasperController {
 
     @FXML
-    void customerButtonOnAction(ActionEvent event) {
+    void customerButtonOnAction(ActionEvent event) throws JRException {
+        JasperDesign design = JRXmlLoader.load("src/main/resources/report/Customer_List_report.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(design);
+        JasperPrint jasperPrint = null;
+        try {
+            jasperPrint = JasperFillManager.fillReport(jasperReport, null, DBConnection.getInstance().getConnection());
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        JasperViewer.viewReport(jasperPrint,false);
 
     }
 
