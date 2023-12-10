da74b0a3cb1d5c43a4bd68f1c1b84f7f7f4b6307
Thimira Aloka M H <150279156+ThimiraAloka@users.noreply.github.com>
2023-12-10 21:19:43 +0530

    Create  FXML dashbord and create jasper view

---
+++ Jasper_Customer_Item_List_iCM105132/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+j
+:src/main/java/controller/DashboardForJasperController.java,5\2\528d3c0901a4ad8cf64e3c6c1a6d8efc481c996f
---
+++ Jasper_Customer_Item_List_iCM105132/src/main/java/controller/DashboardForJasperController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardForJasperController {
+
+    @FXML
+    void customerButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void itemButtonOnAction(ActionEvent event) {
+
+    }
+
+}
